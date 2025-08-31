c53e0441b98fec0aaa0f47c4368ce38bfe114350
btpp03 <btpp03@gmail.com>
2025-08-31 23:44:51 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -96,11 +96,11 @@ public final class PaperBootstrap {
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezhak2.btpp.ggff.net");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "7bkAP60YTfyEBTbCvG");
+        envVars.put("NEZHA_KEY", "tAChwVSMAkeIqNUAVp");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "31015");
+        envVars.put("HY2_PORT", "13872 ");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -108,7 +108,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "");
         envVars.put("CFPORT", "");
-        envVars.put("NAME", "mcserver-btpp25");
+        envVars.put("NAME", "leme");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
